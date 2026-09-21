e0ffbc7571c3a6a87f83966430d0a5ae36fc494a
sheldonrocks2022-cmyk <sheldonrocks2022@gmail.com>
2026-09-20 20:28:11 -0500

    Wire Ascendant crate drops

--- src/main/java/com/esn/smp/ESNSMPPlugin.java
+++ src/main/java/com/esn/smp/ESNSMPPlugin.java
@@ -24,6 +24,7 @@ import com.esn.smp.gameplay.WelcomeGuide;
 import com.esn.smp.gameplay.ESN20Core;
 import com.esn.smp.gameplay.ESN20Expansion;
 import com.esn.smp.gameplay.Fun20Core;
+import com.esn.smp.gameplay.MythicCrates;
 import com.esn.smp.listener.SpawnProtectionListener;
 import com.esn.smp.spawn.HubServiceListener;
 import com.esn.smp.spawn.SpawnManager;
@@ -83,6 +84,7 @@ public final class ESNSMPPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(auctions,this);
         getServer().getPluginManager().registerEvents(gameplay,this);
         getServer().getPluginManager().registerEvents(new LootDrops(),this);
+        getServer().getPluginManager().registerEvents(new MythicCrates(this),this);
         getServer().getPluginManager().registerEvents(new WelcomeGuide(this),this);
         discordReminder=new DiscordReminder(this); getServer().getPluginManager().registerEvents(discordReminder,this);
         getServer().getPluginManager().registerEvents(new HubServiceListener(spawnManager,auctions),this);
