864e885e085d2055726fa619791134e5ef125369
sheldonrocks2022-cmyk <sheldonrocks2022@gmail.com>
2026-09-20 21:44:29 -0500

    Register mega crate drops

--- src/main/java/com/esn/smp/ESNSMPPlugin.java
+++ src/main/java/com/esn/smp/ESNSMPPlugin.java
@@ -26,6 +26,7 @@ import com.esn.smp.gameplay.ESN20Expansion;
 import com.esn.smp.gameplay.Fun20Core;
 import com.esn.smp.gameplay.MythicCrates;
 import com.esn.smp.gameplay.ExtendedCrates;
+import com.esn.smp.gameplay.MegaCrates;
 import com.esn.smp.listener.SpawnProtectionListener;
 import com.esn.smp.spawn.HubServiceListener;
 import com.esn.smp.spawn.SpawnManager;
@@ -87,6 +88,7 @@ public final class ESNSMPPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new LootDrops(),this);
         getServer().getPluginManager().registerEvents(new MythicCrates(this),this);
         getServer().getPluginManager().registerEvents(new ExtendedCrates(),this);
+        getServer().getPluginManager().registerEvents(new MegaCrates(),this);
         getServer().getPluginManager().registerEvents(new WelcomeGuide(this),this);
         discordReminder=new DiscordReminder(this); getServer().getPluginManager().registerEvents(discordReminder,this);
         getServer().getPluginManager().registerEvents(new HubServiceListener(spawnManager,auctions),this);
